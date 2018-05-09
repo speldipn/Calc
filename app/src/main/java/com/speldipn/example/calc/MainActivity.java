65d35b317034375a614ec481509442247b6a256d
speldipn <speldipn.gmail.com>
2018-05-09 15:22:45 +0900

    first add animation

--- app/src/main/java/com/speldipn/example/calc/MainActivity.java
+++ app/src/main/java/com/speldipn/example/calc/MainActivity.java
@@ -1,8 +1,18 @@
 package com.speldipn.example.calc;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewOutlineProvider;
+import android.view.ViewTreeObserver;
+import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -12,12 +22,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     TextView exp;
     TextView result;
+    TextView aniView;
     String temp = "";
+    float aniView_x, aniView_y, aniView_w, aniView_h;
+    float cur_x, cur_y;
+    ObjectAnimator view1AniX;
+    ObjectAnimator view1AniY;
+    ObjectAnimator viewAniXRotate;
+    ConstraintLayout constraintLayout;
+    View prevView = null;
+    View fakeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fakeView = new View(this);
+        constraintLayout = findViewById(R.id.constraintLayout);
+        constraintLayout.addView(fakeView);
 
         result = findViewById(R.id.result);
         exp = findViewById(R.id.exp);
@@ -35,10 +58,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.btnBack).setOnClickListener(this);
         findViewById(R.id.btnLeftBracket).setOnClickListener(this);
         findViewById(R.id.btnRightBracket).setOnClickListener(this);
+
+        aniView = findViewById(R.id.aniView);
+
+        ViewTreeObserver vto = exp.getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                aniView_x = aniView.getX();
+                aniView_y = aniView.getY();
+                aniView_w = aniView.getWidth();
+                aniView_h = aniView.getHeight();
+            }
+        });
     }
 
     @Override
     public void onClick(View view) {
+        boolean runAni = false;
+
         if (exp.getText().equals("0")) {
             temp = "";
         }
@@ -46,50 +84,65 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (view.getId()) {
             case R.id.btn1:
                 temp += "1";
+                runAni = true;
                 break;
             case R.id.btn2:
                 temp += "2";
+                runAni = true;
                 break;
             case R.id.btn3:
                 temp += "3";
+                runAni = true;
                 break;
             case R.id.btn4:
                 temp += "4";
+                runAni = true;
                 break;
             case R.id.btn5:
                 temp += "5";
+                runAni = true;
                 break;
             case R.id.btn6:
                 temp += "6";
+                runAni = true;
                 break;
             case R.id.btn7:
                 temp += "7";
+                runAni = true;
                 break;
             case R.id.btn8:
                 temp += "8";
+                runAni = true;
                 break;
             case R.id.btn9:
                 temp += "9";
+                runAni = true;
                 break;
             case R.id.btn0:
                 temp += "0";
+                runAni = true;
                 break;
             case R.id.btnPlus:
                 temp += "+";
+                runAni = true;
                 break;
             case R.id.btnMinus:
                 temp += "-";
+                runAni = true;
                 break;
             case R.id.btnDivide:
                 temp += "/";
+                runAni = true;
                 break;
             case R.id.btnMultiply:
                 temp += "*";
-                break;xx
+                runAni = true;
+                break;
             case R.id.btnCancel:
                 temp = "0";
                 break;
             case R.id.btnDot:
+                runAni = true;
                 if (exp.length() <= 1) {
                     if (exp.equals("0")) {
                         temp += "0.";
@@ -108,9 +161,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
                 break;
             case R.id.btnLeftBracket:
+                runAni = true;
                 temp += "(";
                 break;
             case R.id.btnRightBracket:
+                runAni = true;
                 temp += ")";
                 break;
             case R.id.all:
@@ -119,7 +174,59 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
 
+        if (runAni) {
+            runAnimation(view);
+        }
+
         exp.setText(temp);
+    }
+
+    public void runAnimation(View view) {
+        int left = view.getLeft();
+        int right = view.getRight();
+        int top = view.getTop();
+        int bottom = view.getBottom();
+        fakeView.setLeft(view.getLeft());
+        fakeView.setTop(view.getTop());
+        fakeView.setRight(view.getRight());
+        fakeView.setBottom(view.getBottom());
+        fakeView.setVisibility(View.VISIBLE);
+        fakeView.setBackgroundColor(Color.GREEN);
+
+//
+//        prevView = view;
+//        cur_x = view.getX();
+//        cur_y = view.getY();
+//
+//        view1AniX = ObjectAnimator.ofFloat(view, "X",
+//                (aniView_x - view.getWidth() / 2) + aniView_w / 2);
+//
+//        view1AniY = ObjectAnimator.ofFloat(view, "Y",
+//                (aniView_y - view.getHeight() / 2) + aniView_h / 2);
+//
+//        viewAniXRotate = ObjectAnimator.ofFloat(view, "rotation", 0.0f, 1440f);
+//
+//        AnimatorSet aniSet = new AnimatorSet();
+//        aniSet.playTogether(view1AniX, view1AniY, viewAniXRotate);
+////        aniSet.setInterpolator(new AccelerateDecelerateInterpolator());
+//        aniSet.addListener(new AnimatorListenerAdapter() {
+//            @Override
+//            public void onAnimationEnd(Animator animation) {
+//                if (prevView != null) {
+//                    view1AniX = ObjectAnimator.ofFloat(prevView, "X", cur_x);
+//                    view1AniY = ObjectAnimator.ofFloat(prevView, "Y", cur_y);
+//                    viewAniXRotate = ObjectAnimator.ofFloat(prevView, "rotation", 0.0f, 1440f);
+//                    AnimatorSet aniSet = new AnimatorSet();
+//                    aniSet.playTogether(view1AniX, view1AniY, viewAniXRotate);
+////                    aniSet.setInterpolator(new AccelerateDecelerateInterpolator());
+//                    aniSet.setDuration(200);
+//                    aniSet.start();
+//                }
+//            }
+//        });
+//        aniSet.setDuration(200);
+//        aniSet.start();
+
     }
 
     public String calcParser(String exp) {
